9022b2ea697a58cfd4af62cf6ecaf60a33c7c1a9
saucecake <48312022+DragonMeme@users.noreply.github.com>
2022-06-24 19:20:19 +1200

    Make Swagger more user readable.
    Also remove dead code type checking.

--- data-science/api.py
+++ data-science/api.py
@@ -2,31 +2,33 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from useModel import useModel
 
-root = FastAPI()
+root = FastAPI(title="Data Science API Endpoints!")
 
 class GameGrid(BaseModel):
     GameGrid: list[int]
 
-@root.get("/api/health")
-def replyHealth():
+@root.get(
+    "/api/health",
+    summary="Check whether the API is up or not.",
+    tags=["API"]
+)
+def health_Check():
     return {"message": "I am up and running!"}
 
 @root.post(
     "/api/move",
+    summary="Have the model decide the move for the game.",
     description="""
-    Let the AI decide the best swipe move based on given input.
+    Let the model decide the best swipe move based on given input.
     Input:
-        A list of 16 numbers.
+        A list with 16 numbers. The numbers used are indices of 2.
     Returns:
-        "Up", "Down", "Left" or "Right"
-    """
+        "Up", "Down", "Left", "Right" or "Game Over"
+    """,
+    tags=["API"]
 )
-def decideMove(grid: list[int]):
+def decide_Move_or_Game_Over(grid: list[int]):
     if len(grid) != 16:
         raise HTTPException(status_code=400, detail=f"Invalid array size! Expected 16 numbers, got {len(grid)} numbers instead.")
-
-    for item in grid:
-        if type(item) != int:
-            raise HTTPException(status_code=400, detail="Invalid data type detected!")
     
     return useModel(grid)
