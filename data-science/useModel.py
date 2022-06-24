fb4701668a2b2ef0aeaba610bd5655e7384057e7
saucecake <48312022+DragonMeme@users.noreply.github.com>
2022-06-24 20:03:28 +1200

    Adapt use of keras model to script.
    Include a few sample inputs which is optional for testing.
    
    Make the script load the model beforehand and use model whenever the
    function is called.

--- data-science/useModel.py
+++ data-science/useModel.py
@@ -1,7 +1,25 @@
-import random
+import tensorflow as tf
+import pandas as pd
+
+model = tf.keras.models.load_model('output/my_model')
 
 def useModel(grid: list) -> str:
+    """
+    Attempt to use the model to predict next move or game over state.
+    Returns:
+        string: Model's suggested move or result.
+    """
+    output = list(model.predict(tf.convert_to_tensor(pd.DataFrame([grid], columns=[str(i) for i in range(16)])))[0])
+
+    max_number = max(output)
+    index = output.index(max_number)
+
+    map_dir = ["Up", "Down", "Left", "Right", "Game Over"]
+    return map_dir[index]
 
 
-    map_dir = {0:"Up", 1: "Right", 2: "Down", 3: "Left"}
-    return map_dir[random.randrange(0,4)]
+if __name__ == "__main__":
+    print(model.summary())
+    print(useModel([1,2,3,4,4,3,2,1,1,2,3,4,2,1,2,3])) # Should be Game Over.
+    print(useModel([1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]))
+    print(useModel([1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]))
