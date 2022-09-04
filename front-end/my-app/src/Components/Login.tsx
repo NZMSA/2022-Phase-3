e57fc26ed9d13e245aa8445b334e80861951f20d
Kent Tsou <tsoukent97@gmail.com>
2022-09-04 13:08:19 +1200

    added UI to login page

--- front-end/my-app/src/Components/Login.tsx
+++ front-end/my-app/src/Components/Login.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react"; 
-
+import { Button, Form, Segment} from "semantic-ui-react"
 function Login() {
+    let userInfo = "";
+
     const initialState = {
-        username: '',
-        password: ''
+        username: "",
+        password: ""
     }
 
     const [form, useForm] = useState(initialState);
@@ -17,44 +19,40 @@ function Login() {
         })
     }
 
+    function handleSubmit (e:any) {
+        e.preventDefault()
+        userInfo = form.username
+        console.log(userInfo)
+    }
+
     return (
-      <form>
-
-        <h3>Login</h3>
-
-          <label>Username</label>
-          <input
-            type="text"
-            className="form-username"
-            placeholder="Username"
-            onChange={handleChange}
-            // value={form.username}
-          />
-
-          <label>Password</label>
-          <input
-            name="password"
-            type="password"
-            className="form-password"
-            placeholder="Password"
-            onChange={handleChange}
-            value={form.password}
-          />
-          
-          <button>
-            Login
-          </button>
-      </form>
+      <Segment inverted>
+        <Form inverted>
+            <Form.Group widths='equal'>
+                <Form.Input
+                    fluid label='Username'
+                    name="username"
+                    type="text"
+                    className="form-username"
+                    placeholder="Username"
+                    onChange={handleChange}
+                    value={form.username}
+                />
+                <Form.Input
+                    fluid label='Password'
+                    name="password"
+                    type="password"
+                    className="form-password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    value={form.password}
+                />
+                </Form.Group>
+                <Button onClick={handleSubmit}>Login</Button>
+        </Form>
+      </Segment>
     )
   }
 
 export default Login;
 
-// export function loginCustomer (customer) {
-//     return request.post('/login')
-//       .send({
-//         username: customer.username,
-//         password: customer.password
-//       })
-//       .then(res => res.body)
-//   }
