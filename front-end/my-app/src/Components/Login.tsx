5943d99050a703ce47aeb61abe5a7c16b4cca125
Kent Tsou <tsoukent97@gmail.com>
2022-09-06 22:23:36 +1200

    tidy login.tsx up

--- front-end/my-app/src/Components/Login.tsx
+++ front-end/my-app/src/Components/Login.tsx
@@ -34,18 +34,6 @@ function Login() {
             <motion.div 
                 animate={{rotate: [0, 50, -20, 20, -10, 50, -30, 40, 0, 20, -25, 360], scale:2.5}} 
                 transition={{duration: 5}}
-                // animate={{
-                //   scale: [1, 2, 2, 1, 1],
-                //   rotate: [0, 0, 180, 180, 0],
-                //   borderRadius: ["0%", "0%", "50%", "50%", "0%"]
-                // }}
-                // transition={{
-                //   duration: 2,
-                //   ease: "easeInOut",
-                //   times: [0, 0.2, 0.5, 0.8, 1],
-                //   repeat: Infinity,
-                //   repeatDelay: 1
-                // }}
                 >
                     <div className="form-container">
                         <form>
